2e57571a77078fa9b22338b2f71d476a5128c62b
Romulo Rosa <romulorosadev@gmail.com>
2024-09-20 13:15:41 -0300

    exercicios resolvidos

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -8,26 +8,59 @@ const {
 } = require("../src/index");
 
 // Testes para a função somar
-test("Soma dois números corretamente", () => {});
+test("Soma dois números corretamente", () => {
+  expect(somar(10, 20)).toBe(30)
+});
 
-test("Soma dois números negativos corretamente", () => {});
+test("Soma dois números negativos corretamente", () => {
+  expect(somar(-5, -20)).toBe(-25)
+});
 
 // Testes para a função subtrair
-test("Subtrai dois números corretamente", () => {});
+test("Subtrai dois números corretamente", () => {
+  expect(subtrair(20, 5)).toBe(15)
+});
 
-test("Subtrai com resultado negativo", () => {});
+test("Subtrai com resultado negativo", () => {
+  expect(subtrair(20, -25)).toBe(-5)
+});
 
 // Testes para a função multiplicar
-test("Multiplica dois números corretamente", () => {});
+test("Multiplica dois números corretamente", () => {
+  expect(multiplicar(5, 10)).toBe(50)
+});
 
-test("Multiplica número por zero", () => {});
+test("Multiplica número por zero", () => {
+  expect(multiplicar(5, 0)).toBe(0)
+});
 
 // Testes para a função dividir
-test("Divide dois números corretamente", () => {});
+test("Divide dois números corretamente", () => {
+  expect(dividir(20, 10)).toBe(2)
+});
 
-test("Lança erro ao dividir por zero", () => {});
+test("Lança erro ao dividir por zero", () => {
+  expect(
+    () => {
+      dividir(2, 0)
+    }
+  ).toThrow("Erro");
+});
 
 // Testes para a função ehPar
-test("Verifica se o número é par", () => {});
+test("Verifica se o número é par", () => {
+  expect(ehPar(20)).toBe(true)
+});
 
-test("Verifica se o número é ímpar", () => {});
+test("Verifica se o número é ímpar", () => {
+  expect(ehPar(21)).toBe(false)
+});
+
+//Testes para a função encontrarMaior
+test("Verifica A é maior que B", () => {
+  expect(encontrarMaior(10, 1)).toBe(10)
+});
+
+test("Verifica B é maior que a", () => {
+  expect(encontrarMaior(5, 50)).toBe(50)
+});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 // Função que soma dois números
-function somar() {}
+function somar(a, b) {
+  return a + b
+}
 
 // Função que subtrai dois números
-function subtrair() {}
-
+function subtrair(a, b) {
+  if (a < 0) {
+    return -a - (+b)
+  }
+  if (b < 0) {
+    return a - (-b)
+  }
+  if (a < 0 && b < 0) {
+    return -a - (-b)
+  }
+  return a - b
+}
 // Função que multiplica dois números
-function multiplicar() {}
+function multiplicar(a, b) {
+  return a * b
+}
 
 // Função que divide dois números, com verificação de divisão por zero
-function dividir() {}
+function dividir(a, b) {
+  if (a === 0 || b === 0) {
+    throw new Error("Erro")
+  }
+  return a / b
+}
 
 // Função que verifica se um número é par
-function ehPar() {}
+function ehPar(a) {
+  return a % 2 === 0
+}
+
+// Função que verifica se um número é maior que  outro
+function encontrarMaior(a, b) {
+  if (a > b) {
+    return a
+  } else if (b > a) {
+    return b
+  } else {
+    throw new Error("Error")
+  }
+}
+
 
 // Exporta todas as funções para serem usadas nos testes
 module.exports = {
